10bf226e734f44c5233af20d705eca8ffd31ddd2
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-01-31 00:20:27 +0530

    response added

--- TextSummarization/app.py
+++ TextSummarization/app.py
@@ -1,6 +1,7 @@
 # text summarisation with python
 
 #importing library
+from flask.wrappers import Request
 import nltk
 import string 
 from heapq import nlargest
@@ -48,9 +49,17 @@ def summaryFunc(text):
 text = "There are two recognized subspecies of tiger*: the continental (Panthera tigris tigris) and the Sunda (Panthera tigris sondaica). The largest of all the Asian big cats, tigers rely primarily on sight and sound rather than smell for hunting. They typically hunt alone and stalk prey. A tiger can consume more than 80 pounds of meat at one time. On average, tigers give birth to two to four cubs every two years. If all the cubs in one litter die, a second litter may be produced within five months.Tigers generally gain independence at around two years of age and attain sexual maturity at age three or four for females and four or five years for males. Juvenile mortality is high, however—about half of all cubs do not survive more than two years. Tigers have been known to reach up to 20 years of age in the wild.Males of the larger subspecies, the continental tiger, may weigh up to 660 pounds. For males of the smaller subspecies—the Sunda tiger—the upper range is at around 310 pounds. Within both subspecies, males are heavier than females.Tigers are mostly solitary, apart from associations between mother and offspring. Individual tigers have a large territory, and the size is determined mostly by the availability of prey. Individuals mark their domain with urine, feces, rakes, scrapes, and vocalizing.Across their range, tigers face unrelenting pressures from poaching, retaliatory killings, and habitat loss. They are forced to compete for space with dense and often growing human populations. "
 
 @app.route('/')
-def hello_world():
-    # return summaryFunc(text)
+def home():
     return render_template('index.html')
+
+@app.route('/predict', methods =["POST","GET"])
+def predict():
+    if Request.method=='POST':
+        text = str(Request.form['passage'])
+        sum = summaryFunc(text)
+        return render_template('index.html', sum = sum)
+    else:
+        return("oops something crashed")
 
 # print(summaryFunc(text))
 
