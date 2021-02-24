c7faf05017fca38ca5420f504387965506a83b17
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-24 18:54:22 +0530

    django sample page loaded

--- MileagePredictor/Webapp/Webapp/urls.py
+++ MileagePredictor/Webapp/Webapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from mileage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^$',views.index,name = 'Homepage')
 ]
