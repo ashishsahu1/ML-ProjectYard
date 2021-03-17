c7470c32d64617372790aabff263b3ec081529b8
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2021-03-17 23:08:30 +0530

    created Twitter Sentimental Analysis WebApp -Shantam

---
+++ Twitter Sentimental Analysis WebApp/Model/Model.py
@@ -0,0 +1,76 @@
+# this is a local Credential Helper
+import TwitterBasedSentimentalAnalysisWebApp.CredentialHelper as twitterCredential
+
+# Tweepy is a lib that can be used to connect to twitter api
+
+import tweepy
+
+# helps us to make sentimental analysis on the given data on bases of text
+
+from textblob import TextBlob
+
+
+class model(object):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def get_authenticated_api():
+        auth = tweepy.OAuthHandler(twitterCredential.candidate_key, twitterCredential.candidate_sec)
+        auth.set_access_token(twitterCredential.access_key, twitterCredential.access_sec)
+        api = tweepy.API(auth)
+        return api
+
+    def get_user_name(self):
+        api = self.get_authenticated_api()
+        user = api.me()
+        return user.name
+
+    def get_live_tweets_from_Twitter(self, text):
+        api = self.get_authenticated_api()
+        # getting the tweets form twitter using The Twitter aap and tweepy
+        # Demo of getting text from twitter
+
+        tweet_live = api.search(text)
+        for tweet in tweet_live:
+            tweet_is = tweet.text
+            # for demo added a print statement
+            print(tweet_is)
+        return tweet_live
+
+    def analysis_live_tweet_data(self, text):
+        # Making prediction on the tweet provided by twitter related to this topic
+
+        # lets choose a random topic i choose this as it was in news alot
+
+        # we were abe to get the
+        tweet_live = self.get_live_tweets_from_Twitter(text)
+        for tweet in tweet_live:
+            tweet_is = tweet.text
+            analysis = TextBlob(tweet_is)
+            print(analysis.sentiment)
+
+    def detailed_analysis_tweet_data(self, text):
+        # if polarity is in negative then the tweet is negative
+        # if in positive then its a positive tweet
+        # if polarity is greater then 0 and less then 5 then tweet is neutral
+
+        tweet_live = self.get_live_tweets_from_Twitter(text)
+        for tweet in tweet_live:
+            tweet_is = tweet.text
+            analysis = TextBlob(tweet_is)
+            polarity = TextBlob(tweet_is).sentiment.polarity
+            subjectivity = TextBlob(tweet_is).sentiment.subjectivity
+            print("tweet = ", tweet_is)
+            print("polarity =", polarity)
+            print('*' * 20)
+            print("subjectivity =", subjectivity)
+            print('*' * 20)
+            print('*' * 30)
+
+
+if __name__ == "__main__":
+    ob = model()
+    text = ""
+    ob.get_live_tweets_from_Twitter(text)
