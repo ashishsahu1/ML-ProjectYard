9e6576a6fd7ebf34d938d810aaa30ad787cd2d6a
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-04-03 19:36:16 +0530

    Update code.py

--- Colorful sketch filter/code.py
+++ Colorful sketch filter/code.py
@@ -1,48 +1,32 @@
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt  #imported the required libraries
 import numpy as np
 import cv2
-import os.path
+import os.path 
 
-# take path of the image as input
-img_path = input("Enter the path here:")  #example -> C:\Users\xyz\OneDrive\Desktop\project\image.jpg  
-img = cv2.imread(img_path)
 
-#C:\Users\akrit\OneDrive\Desktop\Ghostifying\p3.jpg
-#C:\Users\akrit\OneDrive\Desktop\GSSOC'21\ghost_image\Images\hehe.jpg
-#C:\Users\akrit\OneDrive\Desktop\Ghostifying\images\c3.jpg
+# take the path of the image as input
+image_path = input("Enter the path here:")  # example -> C:\Users\xyz\OneDrive\Desktop\project\image.jpg  
+img = cv2.imread(image_path)
 
-#plt.imshow(img)
-#plt.show()
+#C:\Users\akrit\OneDrive\Desktop\Ghostifying\images\c7_.jpg
+
 image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#plt.figure(figsize= (10,10))
-#plt.imshow(image)
-#plt.show()
-img_small = cv2.pyrDown(image)
+image_small = cv2.pyrDown(image)
 num_iter = 5
 for _ in range(num_iter):
-    img_small= cv2.bilateralFilter(img_small, d=9, sigmaColor=9, sigmaSpace=7)
-img_rgb = cv2.pyrUp(img_small)
-#plt.imshow(img_rgb)
-#plt.show()
+    image_small= cv2.bilateralFilter(image_small, d=9, sigmaColor=9, sigmaSpace=7)
+image_rgb = cv2.pyrUp(image_small)
+image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2GRAY)
+image_blur = cv2.medianBlur(image_gray, 7)
+image_edge = cv2.adaptiveThreshold(image_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 0.2)
+image_edge = cv2.cvtColor(image_edge, cv2.COLOR_GRAY2RGB)
 
+result = cv2.bitwise_or(image_edge, image)    #used bitwise or method between the image_edge and image
 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-img_blur = cv2.medianBlur(img_gray, 7)
-img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 0.2)
-
-#plt.imshow(img_edge)
-#plt.show()
-img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-#plt.imshow(img_edge)
-#plt.show()
-# img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-     
-array = cv2.bitwise_or(img_edge, image)    #used bitwise or method
-#array = cv2.bitwise_not(image, img_edge) 
 plt.figure(figsize= (10,10))
-plt.imshow(array)
+plt.imshow(result)
 plt.axis('off')
-filename = os.path.basename(img_path)
-plt.savefig("(Filtered)"+filename)  #saved file name as (Filtered)image_name.jpg
+filename = os.path.basename(image_path)
+plt.savefig("(Colorful-Sketch Filtered)"+filename)  #saved file name as (Colorful-Sketch Filtered)image_name.jpg/png etc
 
-plt.show()  #final filtered photo
+plt.show()  #final output is a colorful sketch filtered photo
