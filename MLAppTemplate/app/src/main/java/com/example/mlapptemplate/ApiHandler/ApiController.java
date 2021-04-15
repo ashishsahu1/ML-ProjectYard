378255eba7bed8e6c62cc1c8d5b24298b6b6008e
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2021-04-15 14:47:51 +0530

    text based - ML Android App Template for multipurpose API's

--- MLAppTemplate/app/src/main/java/com/example/mlapptemplate/ApiHandler/ApiController.java
+++ MLAppTemplate/app/src/main/java/com/example/mlapptemplate/ApiHandler/ApiController.java
@@ -2,6 +2,7 @@ package com.example.mlapptemplate.ApiHandler;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.TextView;
 import com.google.gson.JsonObject;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -10,30 +11,33 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ApiController {
 
     private final String TAG = "ApiController";
+    private final List<String> res = new ArrayList<>();
     private String url;
     private ApiServiceProvider apiServiceProvider;
     private Context context;
-    private List<String> res = new ArrayList<>();
+    private ResponseBody responseBody;
 
     public List<String> getRes() {
         return res;
     }
 
+    public ApiServiceProvider getApiServiceProvider() {
+        return apiServiceProvider;
+    }
 
     public void createSession(String inputUrl) {
         url = inputUrl;
         connect();
     }
 
-    private void connect()
-    {
+    private void connect() {
         Retrofit retrofit = new Retrofit.Builder().baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
@@ -45,8 +49,8 @@ public class ApiController {
         }
     }
 
-    public void webHookHandler(String inputData) {
-        Log.d(TAG, "webHookHandler: "+inputData);
+    public void webHookHandler(String inputData, TextView responseBtn) throws IOException {
+        Log.d(TAG, "webHookHandler: " + inputData);
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("data", inputData);
         Call<ResponseBody> ab = apiServiceProvider.sendDataToWebHook(jsonObject);
@@ -56,8 +60,7 @@ public class ApiController {
                 if (response.isSuccessful()) {
                     try {
                         if (response.body() != null) {
-                            res.add(response.body().string());
-                            Log.d(TAG, "onResponse: "+ res.get(0));
+                            responseBtn.setText(response.body().string());
                         }
                     } catch (Exception e) {
                         Log.d(TAG, "onResponse: error while getting the response data /n", e);
@@ -67,7 +70,7 @@ public class ApiController {
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.e(TAG, "onFailure: ",t );
+                Log.e(TAG, "onFailure: ", t);
             }
         });
         Log.d(TAG, "webHookHandler: 11");
--- MLAppTemplate/app/src/main/java/com/example/mlapptemplate/MainActivity.java
+++ MLAppTemplate/app/src/main/java/com/example/mlapptemplate/MainActivity.java
@@ -9,52 +9,69 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.mlapptemplate.ApiHandler.ApiController;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    // log Tag
     private final String TAG = "ApiController";
 
-    private ApiController apiController = new ApiController();
+    // Api Controller Object
+    private final ApiController apiController = new ApiController();
 
-    private Button connectBtn,predictBtn;
-    private EditText urlData,inputData;
+    // Buttons
+    private Button connectBtn, predictBtn;
+
+    // input text buttons
+    private EditText urlData, inputData;
+
+    // Response Text View
     private TextView response;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // buttons
+        // buttons created
         connectBtn = findViewById(R.id.connectBtn);
         predictBtn = findViewById(R.id.predictBtn);
 
 
+        // Connect to API Method
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 urlData = findViewById(R.id.urlEditText);
                 String url = urlData.getText().toString();
-                Log.d("ApiController", "onClick: "+url);
+                Log.d("ApiController", "onClick: " + url);
                 if (!url.isEmpty())
-                apiController.createSession(url);
+                    apiController.createSession(url);
             }
         });
 
+        // Predictor
         predictBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 inputData = findViewById(R.id.inputEditText);
 
-                response = findViewById(R.id.responseView);
-                if (!inputData.getText().toString().isEmpty())
-                {
+                Log.d(TAG, "onClick: Post Btn");
 
-                    apiController.webHookHandler(inputData.getText().toString());
-//                    Log.d(TAG, "onClick: "+apiController.getRes().get(0));
-                    response.setText("r");
+                // response view button
+                response = findViewById(R.id.responseView);
+                if (!inputData.getText().toString().isEmpty()) {
+                    try {
+                        apiController.webHookHandler(inputData.getText().toString(), response);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "onClick: ", e);
+                    }
                 }
             }
         });
+
 
     }
 }
